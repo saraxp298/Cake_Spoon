70c71ce7d7f98a359cb94bb57363792de5665884
saranya <saratechide@gmail.com>
2022-02-09 13:46:01 +0530

    static files created

--- Cake_Spoon/Cake_Spoon/settings.py
+++ Cake_Spoon/Cake_Spoon/settings.py
@@ -50,11 +50,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Cake_Spoon.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    
+    os.path.join(BASE_DIR,'static')
+
+]
+
+STATIC_ROOT=os.path.join(BASE_DIR,'backup')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
