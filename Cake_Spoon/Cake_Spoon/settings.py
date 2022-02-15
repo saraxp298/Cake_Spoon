8ff09b34f3b9469a3ee21f12ccf1be5371160ad2
saranya <saratechide@gmail.com>
2022-02-15 22:49:58 +0530

    load static,Database connected with django(postgresql database,pgadmin application,psycopg2 library).Changed some texts anof index.html page

--- Cake_Spoon/Cake_Spoon/settings.py
+++ Cake_Spoon/Cake_Spoon/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'Cake_Spoon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Cake_Spoondb',
+        'USER':'postgres',
+        'PASSWORD':'saraxp298',
+        'HOST':'localhost'
     }
 }
 
